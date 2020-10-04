acd6d76da80410f50d463362ea75d8ba4b06eb90
Ankit12063 <71066451+Ankit12063@users.noreply.github.com>
2020-10-04 23:16:43 +0530

    Update DifferentSummands.java

--- week3_greedy_algorithms/6_maximum_number_of_prizes/DifferentSummands.java
+++ week3_greedy_algorithms/6_maximum_number_of_prizes/DifferentSummands.java
@@ -1,20 +1,38 @@
-import java.util.*;
-
 public class DifferentSummands {
     private static List<Integer> optimalSummands(int n) {
         List<Integer> summands = new ArrayList<Integer>();
-        //write your code here
-        return summands;
+        int start = 1;
+        int newNumber = n;
+
+        if (n == 2) {
+            summands.add(2);
+            return summands;
+        }
+
+        while (true) {
+            if (summands.contains(newNumber - start)) {
+                start++;
+                continue;
+            } else {
+                newNumber -= start;
+                summands.add(start);
+                start++;
+            }
+
+            if (newNumber == 0) {
+                return summands;
+            }
+        }
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         List<Integer> summands = optimalSummands(n);
         System.out.println(summands.size());
+
         for (Integer summand : summands) {
             System.out.print(summand + " ");
         }
     }
 }
-
